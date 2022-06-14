f308110947ca9396d645e7fc70f06716cf89b2ef
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-06-14 16:35:38 +0100

    inquirer imported + game setting Qs + init fn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello");
+//ask username question
+const inquirer = require("inquirer");
+
+const gameSettingsQuestions = [
+  {
+    type: "input",
+    message: "Please enter your nickname",
+    name: "nickname",
+  },
+];
+//ask the category question and consoel log data from the chosen category
+const init = async () => {
+  //prompt the game setting questions and store answers
+  const answers = await inquirer.prompt(gameSettingsQuestions);
+  //log the answers
+  console.log(answers);
+};
+init();
+
+//00:37:44 timestamp video
